d04c8eb39d18b1d5e3c2da0b2e3562b87e830e89
Marcel <me@m4rc3l.de>
2023-07-09 14:17:00 +0200

    updated nix

--- derivation.nix
+++ derivation.nix
@@ -5,9 +5,17 @@ mkYarnPackage {
 
     buildInputs = [ yarn ];
     postPatch = ''
+      substituteInPlace nuxt.config.json \
+        --replace 'http://localhost:3000' 'https://${domain}'
     '';
 
     buildPhase = ''
+      FILE=$(readlink ./deps/dd-ix-website/node_modules)
+      rm ./deps/dd-ix-website/node_modules
+      mkdir ./deps/dd-ix-website/node_modules
+      cp -r $FILE/ ./deps/dd-ix-website/
+      cp -r ./node_modules/* ./deps/dd-ix-website/node_modules/
+
       yarn run generate
     '';
 
