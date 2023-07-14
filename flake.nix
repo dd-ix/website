1b6d48b8f1c5a3bf7204081d634fb3d9cffcd8a3
revol-xut <revol-xut@protonmail.com>
2023-07-14 23:24:29 +0200

    add output nixosModule

--- flake.nix
+++ flake.nix
@@ -1,23 +1,50 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # needed to set up the schema in test vm database
+    tlms-rs = {
+      url = "github:tlm-solutions/tlms.rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
+  outputs = inputs@{ self, nixpkgs, naersk, tlms-rs, utils, ... }:
     utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          package-production = pkgs.callPackage ./derivation.nix {
+
+          package = pkgs.callPackage ./derivation.nix {
             domain = "dd-ix.net";
           };
+
+          test-vm-pkg = self.nixosConfigurations.presence-mctest.config.system.build.vm;
+
         in
         rec {
           checks = packages;
           packages = {
-            presence = package-production;
-            default = package-production;
+            presence = package;
+            default = package;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = (with packages.default; nativeBuildInputs ++ buildInputs) ++ [
+              # python for running test scripts
+              (pkgs.python3.withPackages (p: with p; [
+                requests
+              ]))
+            ];
           };
         }
       ) // {
@@ -25,5 +52,11 @@
         inherit (self.packages.${prev.system})
           presence;
       };
+
+      nixosModules = rec {
+        default = presence;
+        presence = import ./nixos-module;
+      };
+
     };
 }
