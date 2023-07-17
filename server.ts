5db077392a3aa1e699bb8a4433377b25733bcbff
Marcel <me@m4rc3l.de>
2023-07-17 17:33:53 +0200

    Added language redirects

--- server.ts
+++ server.ts
@@ -13,12 +13,40 @@ async function main() {
   const port = process.env['PORT'] ?? 4000;
   const appDir = process.env['APP_DIR'] ?? ".";
 
-  const server = express();
+  const server: express.Express = express();
 
-  const [de, en] = await Promise.all([app(appDir, "de"), app(appDir, "en")]);
+  const [de, en] = await Promise.all([
+    app(appDir, "de"),
+    app(appDir, "en")
+  ]);
 
   server.use("/de/", de);
   server.use("/en/", en);
+
+  server.all("*", (req, res) => {
+    const languages = req.acceptsLanguages();
+    let target = 'en';
+
+    if (languages) {
+      for (let language of languages) {
+        if (language.indexOf('-') == 2) {
+          language = language.substring(0, 2);
+        }
+
+        language = language.toLowerCase();
+
+        if (language === 'de') {
+          target = 'de';
+          break;
+        } else if (language === 'en') {
+          target = 'en';
+          break;
+        }
+      }
+    }
+
+    res.redirect(`/${target}${req.url}`)
+  });
 
   server.listen(port, () => console.log(`Listening on http://127.0.0.1:${port}...`));
 }
