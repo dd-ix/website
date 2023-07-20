2288b7cf25901e2cf98e71f3a7d561e4f8526df3
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2023-07-20 21:03:58 +0200

    Added document download

--- src/app/pages/documents/documents.component.ts
+++ src/app/pages/documents/documents.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DocumentsService} from "../../api/documents.service";
+import {Document} from "../../api/documents.domain";
 
 @Component({
   selector: 'app-documents',
@@ -17,4 +18,9 @@ export class DocumentsComponent {
     private readonly documentsService: DocumentsService
   ) {
   }
+
+  getDownloadLink(document: Document): string {
+    return `http://127.0.0.1:8080/documents/download/${document.filename}`;
+  }
+
 }
