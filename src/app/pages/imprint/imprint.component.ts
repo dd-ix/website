4f428b21f53dc5400c631b9b2a0a7c0d38dae0a9
Marcel <me@m4rc3l.de>
2023-07-28 17:26:14 +0200

    Added text blocks to privacy policy and imprint

--- src/app/pages/imprint/imprint.component.ts
+++ src/app/pages/imprint/imprint.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, SecurityContext} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from "@angular/router";
+import {TextBlocksService} from "../../api/text-blocks.service";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-imprint',
@@ -11,4 +13,15 @@ import {RouterLink} from "@angular/router";
 })
 export class ImprintComponent {
 
+  protected readonly imprint = this.textBlocksService.getTextBlock("imprint");
+
+  constructor(
+    private readonly textBlocksService: TextBlocksService,
+    private readonly sanitizer: DomSanitizer,
+  ) {
+  }
+
+  protected safeHtml(html: string): string | null {
+    return this.sanitizer.sanitize(SecurityContext.HTML, html);
+  }
 }
--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, SecurityContext} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {TextBlocksService} from "../../api/text-blocks.service";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-privacy-policy',
@@ -10,4 +12,15 @@ import {CommonModule} from '@angular/common';
 })
 export class PrivacyPolicyComponent {
 
+  protected readonly privacyPolicy = this.textBlocksService.getTextBlock("privacy-policy");
+
+  constructor(
+    private readonly textBlocksService: TextBlocksService,
+    private readonly sanitizer: DomSanitizer,
+  ) {
+  }
+
+  protected safeHtml(html: string): string | null {
+    return this.sanitizer.sanitize(SecurityContext.HTML, html);
+  }
 }
