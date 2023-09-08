68dfc95b398037e2acdb7885584fa29e7fafca8f
Marcel <me@m4rc3l.de>
2023-09-08 22:26:59 +0200

    Updated association page

---
+++ src/app/icons/icon-talking/icon-talking.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IconTalkingComponent } from './icon-talking.component';
+
+describe('IconTalkingComponent', () => {
+  let component: IconTalkingComponent;
+  let fixture: ComponentFixture<IconTalkingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [IconTalkingComponent]
+    });
+    fixture = TestBed.createComponent(IconTalkingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
