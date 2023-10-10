fc5e4cdf921b50b56c446a54ecf058af2bea97ea
Marcel <me@m4rc3l.de>
2023-10-10 18:34:53 +0200

    fixed routing

--- src/app/pages/association/association-routing.module.ts
+++ src/app/pages/association/association-routing.module.ts
@@ -1,12 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AssociationComponent} from "./association.component";
-import {TechnicalComponent} from "./workinggroup/technical/technical.component";
 
 const routes: Routes = [
   {path: '', component: AssociationComponent},
-  {path: 'technical', loadChildren: () => import('./workinggroup/technical/technical.module').then(m => m.TechnicalModule) },
-  {path: 'public_relations', loadComponent: () => import('./workinggroup/public-relations/public-relations.component').then(m => m.PublicRelationsComponent) },
+  {
+    path: 'technical',
+    data: {title: $localize`Technical Workinggroup`},
+    loadChildren: () => import('./workinggroup/technical/technical.module').then(m => m.TechnicalModule)
+  },
+  {
+    path: 'public-relations',
+    data: {title: $localize`Public Relations Workinggroup`},
+    loadComponent: () => import('./workinggroup/public-relations/public-relations.component').then(m => m.PublicRelationsComponent)
+  },
+  {
+    path: 'client-and-sponsors',
+    data: {title: $localize`Client and Sponsors Workinggroup`},
+    loadComponent: () => import('./workinggroup/clients-and-sponsors/clients-and-sponsors.component').then(m => m.ClientsAndSponsorsComponent),
+  }
 ];
 
 @NgModule({
