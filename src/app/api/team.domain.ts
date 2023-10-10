5056b0bca869f61d9830d07c0228a0ec8b39a2d6
revol-xut <revol-xut@protonmail.com>
2023-10-10 19:46:01 +0200

    adding working groups to teams

--- src/app/api/team.domain.ts
+++ src/app/api/team.domain.ts
@@ -1,3 +1,58 @@
+
+export enum WorkingGroup {
+  Tech = "Tech",
+  Network = "Network",
+  Services = "Services",
+  DevOps = "DevOps",
+  Events = "Events",
+  FinanceAndLegal = "FinanceAndLegal",
+  ClientsAndSponsors = "ClientsAndSponsors",
+  PublicRelations = "PublicRelations"
+}
+
+function workingGroupPage(working_group: WorkingGroup) : string {
+  switch (working_group) {
+    case WorkingGroup.Tech:
+      return "/association/tech"
+    case WorkingGroup.Network:
+      return "/association/technical"
+    case WorkingGroup.Services:
+      return "/association/technical"
+    case WorkingGroup.DevOps:
+      return "/association/technical"
+    case WorkingGroup.Events:
+      return "/association/events"
+    case WorkingGroup.FinanceAndLegal:
+      return "/association/finance-and-legal"
+    case WorkingGroup.ClientsAndSponsors:
+      return "/association/clients-and-sponsors"
+    case WorkingGroup.PublicRelations:
+      return "/association/public-relations"
+  }
+}
+
+function displayName(working_group: WorkingGroup) : string {
+  switch (working_group) {
+    case WorkingGroup.Tech:
+      return "Tech"
+    case WorkingGroup.Network:
+      return "Network"
+    case WorkingGroup.Services:
+      return "Services"
+    case WorkingGroup.DevOps:
+      return "DevOps"
+    case WorkingGroup.Events:
+      return "Events"
+    case WorkingGroup.FinanceAndLegal:
+      return "Finance and Legal"
+    case WorkingGroup.ClientsAndSponsors:
+      return "Clients and Sponsors"
+    case WorkingGroup.PublicRelations:
+      return "Public Relations"
+  }
+}
+
+
 export interface TeamMember {
   nick: string | null;
   name: string;
@@ -6,6 +61,7 @@ export interface TeamMember {
   ripe_handle: string | null;
   socials: Socials;
   vorstand: boolean;
+  teams: WorkingGroup[]
 }
 
 export interface Socials {
