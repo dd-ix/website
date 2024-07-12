528183a5c8fd2db802161bb13634f20ccdf6ccdb
tanneberger <revol-xut@protonmail.com>
2024-07-12 22:28:20 +0200

    add terms of service to website

--- src/app/pages/network/join/join.component.ts
+++ src/app/pages/network/join/join.component.ts
@@ -3,6 +3,8 @@ import {ButtonComponent} from "@feel/form";
 import {CardComponent} from "../../../core/card/card.component";
 import {IconSendComponent} from "../../../icons/icon-send/icon-send.component";
 import {IconPopMapComponent} from "../../../icons/icon-pop-map/icon-pop-map.component";
+import {API_URL} from "../../../api/api.domain";
+import {Document} from "../../../api/documents.domain";
 
 @Component({
   selector: 'app-join',
@@ -18,4 +20,14 @@ import {IconPopMapComponent} from "../../../icons/icon-pop-map/icon-pop-map.comp
 })
 export class JoinComponent {
 
+  protected readonly API_URL = API_URL;
+
+
+  protected buildDownloadLinkTermsOfService(): string {
+    return new URL(`${API_URL}/documents/download/DD-IX TermsofService.pdf`).toString();
+  }
+
+  protected buildBackgroundImageTermsofService(): string {
+    return new URL(`${API_URL}/documents/download/DD-IX TermsofService.webp`).toString();
+  }
 }
