51b6ea50d38a1ea00a1a98cda922b28a59b6a3f8
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2024-08-22 22:01:29 +0200

    listmonk: remove (#87)

--- src/app/pages/blog/blog.routes.ts
+++ src/app/pages/blog/blog.routes.ts
@@ -5,11 +5,7 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import("./blog-list/blog-list.component").then(c => c.BlogListComponent),
   },
-  {
-    path: 'subscribe',
-    loadComponent: () => import("./blog-subscribe/blog-subscribe.component").then(c => c.BlogSubscribeComponent)
-  },
-  {
+   {
     path: ':slug',
     loadComponent: () => import("./blog-post/blog-post.component").then(c => c.BlogPostComponent)
   },
