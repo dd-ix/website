69efe8884c46d68dee3327639bd70bf9552b66c8
Marcel <me@m4rc3l.de>
2024-12-01 02:17:04 +0100

    home: also list blog posts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,41 +1,48 @@
-import {Component} from '@angular/core';
-import {AsyncPipe, CommonModule, NgOptimizedImage} from '@angular/common';
-import {BlogService} from "../../api/blog.service";
-import {RouterLink} from "@angular/router";
-import {IconGithubComponent} from "../../icons/icon-github/icon-github.component";
-import {IconMastodonComponent} from "../../icons/icon-mastodon/icon-mastodon.component";
-import {IconLinkedinComponent} from "../../icons/icon-linkedin/icon-linkedin.component";
-import {map, share, switchMap} from "rxjs";
-import {IconIbhComponent} from "../../icons/icon-ibh/icon-ibh.component";
-import {IconBcixComponent} from "../../icons/icon-bcix/icon-bcix.component";
-import {IconTudComponent} from "../../icons/icon-tud/icon-tud.component";
-import {CardComponent} from "../../core/card/card.component";
-import {BlogCardComponent} from "../../core/blog-card/blog-card.component";
-import {TextBlockComponent} from "../../core/text-block/text-block.component";
-import {IconFrauenkircheComponent} from "../../icons/icon-frauenkirche/icon-frauenkirche.component";
-import {IconInternetComponent} from "../../icons/icon-internet/icon-internet.component";
-import {IconSocialComponent} from "../../icons/icon-social/icon-social.component";
-import {IconSendComponent} from "../../icons/icon-send/icon-send.component";
-import {ButtonComponent} from "@feel/form";
-import {IconDsiComponent} from "../../icons/icon-dsi/icon-dsi.component";
-import {EventCardComponent} from "../../core/event-card/event-card.component";
-import {IconInterLinkComponent} from "../../icons/icon-inter-link/icon-inter-link.component";
+import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule, NgOptimizedImage } from '@angular/common';
+import { BlogService } from "../../api/blog.service";
+import { RouterLink } from "@angular/router";
+import { IconGithubComponent } from "../../icons/icon-github/icon-github.component";
+import { IconMastodonComponent } from "../../icons/icon-mastodon/icon-mastodon.component";
+import { IconLinkedinComponent } from "../../icons/icon-linkedin/icon-linkedin.component";
+import { combineLatest, map, share, switchMap } from "rxjs";
+import { IconIbhComponent } from "../../icons/icon-ibh/icon-ibh.component";
+import { IconBcixComponent } from "../../icons/icon-bcix/icon-bcix.component";
+import { IconTudComponent } from "../../icons/icon-tud/icon-tud.component";
+import { CardComponent } from "../../core/card/card.component";
+import { BlogCardComponent } from "../../core/blog-card/blog-card.component";
+import { TextBlockComponent } from "../../core/text-block/text-block.component";
+import { IconFrauenkircheComponent } from "../../icons/icon-frauenkirche/icon-frauenkirche.component";
+import { IconInternetComponent } from "../../icons/icon-internet/icon-internet.component";
+import { IconSocialComponent } from "../../icons/icon-social/icon-social.component";
+import { IconSendComponent } from "../../icons/icon-send/icon-send.component";
+import { ButtonComponent } from "@feel/form";
+import { IconDsiComponent } from "../../icons/icon-dsi/icon-dsi.component";
+import { EventCardComponent } from "../../core/event-card/event-card.component";
+import { IconInterLinkComponent } from "../../icons/icon-inter-link/icon-inter-link.component";
 
 @Component({
-    selector: 'app-home',
-    imports: [CommonModule, RouterLink, IconGithubComponent, IconLinkedinComponent, IconMastodonComponent, IconIbhComponent, IconBcixComponent, IconTudComponent, CardComponent, BlogCardComponent, TextBlockComponent, NgOptimizedImage, IconFrauenkircheComponent, IconInternetComponent, IconSocialComponent, IconSendComponent, ButtonComponent, IconDsiComponent, AsyncPipe, EventCardComponent, IconInterLinkComponent],
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  imports: [CommonModule, RouterLink, IconGithubComponent, IconLinkedinComponent, IconMastodonComponent, IconIbhComponent, IconBcixComponent, IconTudComponent, CardComponent, BlogCardComponent, TextBlockComponent, NgOptimizedImage, IconFrauenkircheComponent, IconInternetComponent, IconSocialComponent, IconSendComponent, ButtonComponent, IconDsiComponent, AsyncPipe, EventCardComponent, IconInterLinkComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
   protected readonly events = this.blogService.getUpComingEvents()
     .pipe(map(events => events.slice(0, 3)), share());
 
   protected readonly news_and_event_posts = this.events
-    .pipe(switchMap(events => this.blogService.getNewsPosts()
-        .pipe(map(posts => posts.slice(0, 3 - events.length)))
+    .pipe(
+      switchMap(events => combineLatest({ news: this.blogService.getNewsPosts(), blog: this.blogService.getBlogPosts() })
+        .pipe(map(({ news, blog }) => {
+          const posts = [...news, ...blog];
+          posts.sort((a, b) => Date.parse(b.published) - Date.parse(a.published));
+          return posts.slice(0, 3 - events.length)
+        }
+        ))
       )
-    )
+    );
 
   protected readonly blog_posts = this.blogService.getBlogPosts();
 
