4b97bad0bfd8ce26f5d84500a1ca36ec0a43ad05
tanneberger <github@tanneberger.me>
2024-12-19 03:01:09 +0100

    remove resiliance page

--- src/app/pages/peering/peering-landingpage/peering-landingpage.component.ts
+++ src/app/pages/peering/peering-landingpage/peering-landingpage.component.ts
@@ -4,17 +4,23 @@ import {ButtonComponent} from "@feel/form";
 import {IconSendComponent} from "../../../icons/icon-send/icon-send.component";
 import {IconPopMapComponent} from "../../../icons/icon-pop-map/icon-pop-map.component";
 import {API_URL} from "../../../api/api.domain";
+import {IconAntennaComponent} from "../../../icons/icon-antenna/icon-antenna.component";
+import {IconDirectionsComponent} from "../../../icons/icon-directions/icon-directions.component";
+import {IconSwitchComponent} from "../../../icons/icon-switch/icon-switch.component";
 
 @Component({
     selector: 'app-peering-landingpage',
     templateUrl: './peering-landingpage.component.html',
     styleUrls: ['./peering-landingpage.component.scss'],
-    imports: [
-        CardComponent,
-        ButtonComponent,
-        IconSendComponent,
-        IconPopMapComponent,
-    ]
+  imports: [
+    CardComponent,
+    ButtonComponent,
+    IconSendComponent,
+    IconPopMapComponent,
+    IconAntennaComponent,
+    IconDirectionsComponent,
+    IconSwitchComponent,
+  ]
 })
 export class PeeringLandingpageComponent {
   protected readonly API_URL = API_URL;
