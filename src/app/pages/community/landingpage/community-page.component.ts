05e3a6fa0862fdbf47dfa28243c9114f8a410279
tanneberger <github@tanneberger.me>
2025-09-30 18:20:53 +0200

    app: add community page

--- src/app/pages/community/landingpage/community-page.component.ts
+++ src/app/pages/community/landingpage/community-page.component.ts
@@ -7,11 +7,17 @@ import {API_URL} from "../../../api/api.domain";
 import {IconAntennaComponent} from "../../../icons/icon-antenna/icon-antenna.component";
 import {IconDirectionsComponent} from "../../../icons/icon-directions/icon-directions.component";
 import {IconSwitchComponent} from "../../../icons/icon-switch/icon-switch.component";
+import {IconInterLinkComponent} from "../../../icons/icon-inter-link/icon-inter-link.component";
+import {IconLinuxComponent} from "../../../icons/icon-linux/icon-linux.component";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {TeamMemberComponent} from "../../team/team-member/team-member.component";
+import {IconGitComponent} from "../../../icons/icon-git/icon-git.component";
+import {IconFediComponent} from "../../../icons/icon-fedi/icon-fedi.component";
 
 @Component({
     selector: 'app-landingpage',
-    templateUrl: './peering-landingpage.component.html',
-    styleUrls: ['./peering-landingpage.component.scss'],
+    templateUrl: './community-page.component.html',
+    styleUrls: ['./community-page.component.scss'],
   imports: [
     CardComponent,
     ButtonComponent,
@@ -20,17 +26,15 @@ import {IconSwitchComponent} from "../../../icons/icon-switch/icon-switch.compon
     IconAntennaComponent,
     IconDirectionsComponent,
     IconSwitchComponent,
+    IconInterLinkComponent,
+    IconLinuxComponent,
+    AsyncPipe,
+    NgForOf,
+    TeamMemberComponent,
+    IconGitComponent,
+    IconFediComponent,
   ]
 })
-export class PeeringLandingpageComponent {
+export class CommunityPageComponent {
   protected readonly API_URL = API_URL;
-
-
-  protected buildDownloadLinkTermsOfService(): string {
-    return new URL(`${API_URL}/documents/download/DD-IX_TermsofService.pdf`).toString();
-  }
-
-  protected buildBackgroundImageTermsofService(): string {
-    return new URL(`${API_URL}/documents/download/DD-IX_TermsofService.webp`).toString();
-  }
 }
